03d376485f01b2265f509981459289a9f457276d
SHAIK.SOHAIL <121076079+shaiksohail11@users.noreply.github.com>
2023-03-30 12:47:27 +0530

    Update provider.tf

--- s3_bucket/provider.tf
+++ s3_bucket/provider.tf
@@ -5,9 +5,17 @@ terraform {
       version = "~> 4.0"
     }
   }
+  # here we have to create s3 bucket and a DynamoDB table with value LockID 
+  backend "s3" {
+    bucket = "terraformremotebackend"
+    key = "terraform/lockfile"
+    dynamodb_table = "my-table"
+    region = "us-west-2"
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
 }
+
