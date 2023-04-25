012a2b64cc68b1ae32255d66b9997b599dbd4b61
Sohail <sohailshaikj2@gmail.com>
2023-04-25 17:49:40 +0530

    Added docker instance

---
+++ Docker_instance/variable.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    type = string
+  
+}
+
+variable "vpc_cidr_block" {
+    type = string
+  
+}
+
+variable "subnet_cidr_block" {
+    type = string
+  
+}
+
+variable "trigger_num" {
+    type = string
+  
+}
